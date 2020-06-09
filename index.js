f1c6dc34f2ccf3fef14a27a6e4d1e417e84ad47b
SeyiAriyo <ariyoseyiariyo@gmail.com>
2020-06-09 15:20:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 'use strict';
 
 function getVal() {
-  const input = $('.js-input');
+  const input = $('.js-input').val();
+  console.log(input);
   return input;
 }
 function getDogImage() {
   const input = getVal();
+  // https://dog.ceo/api/breeds/image/random/3
   fetch(`https://dog.ceo/api/breeds/image/random/${input}`)
     .then(response => response.json())
     .then((responseJson) => {
-      console.log(responseJson.message);
+      console.log(responseJson);
       return showString(generateString(responseJson.message));
     });
 }
 
 function generateString(message) {
   let string = '';
-  message.forEach(item => string += `<li><img src = "${item}</img></li>`);
+  message.forEach(item => string += `<li><img src = "${item}"/> </li>`);
+  console.log(string);
   return string;
 }
 
